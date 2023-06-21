8edea843dda05a9b68be6e4f26fadbfaa5240e6d
Dinesh <dineshkumarpaliwal83@gmail.com>
2023-06-21 17:36:59 +0530

    word sound functinality added

--- src/Components/Type_logic.js
+++ src/Components/Type_logic.js
@@ -1,8 +1,13 @@
 import { useState, useEffect, useRef } from "react";       //imported for focus on input
 import { generate } from 'random-words';                  //random-words is npm package ....generate()... generate random word
+
+import { useSpeechSynthesis } from 'react-speech-kit';    //libarary for producing sound
 import index from '../index.css'
+import TextField from '@mui/material/TextField';
+
+
 const NUMB_OF_WORDS = 150;                                //max word in paragraph
-const SECOND = 10;                                        //total time
+const SECOND = 60;                                        //total time
 
 
 const Type_logic = () => {
@@ -18,7 +23,11 @@ const Type_logic = () => {
   //for giving color to char while typing 
   const [currCharIndex, setcurrCharIndex] = useState(-1);    //current char of input 
   const [currChar, setCurrChar] = useState("");
-
+ 
+  //for producing sound 
+  const [value, setValue] = useState('');
+  const { speak } = useSpeechSynthesis();     
+ 
 
   useEffect(() => {
     setWords(generatewords())
@@ -69,11 +78,12 @@ const Type_logic = () => {
 
 
 
-
+ 
   function handleKeyDown({ keyCode, key })                //triggered when input is given and it recognize the key 
   {
-
-    if (keyCode === 32) {                                  //if space bar is pressed
+     
+    if (keyCode === 32) {                                //if space bar is pressed
+      speak({ text: value });                            //whenever space pressed word pronounced ;
       checkMatch();
       setcurrInput("");
       setCurrWordsIndex(currWordIndex + 1);
@@ -83,7 +93,8 @@ const Type_logic = () => {
       setcurrCharIndex(currCharIndex - 1);
       setCurrChar("");
     }
-    else {
+    else
+    {
       setcurrCharIndex(currCharIndex + 1);               //for other keypress event
       setCurrChar(key);
     }
@@ -98,16 +109,16 @@ const Type_logic = () => {
     const doesItMatch = wordToCompare === currInput.trim();
     if (doesItMatch) setCorrect(Correct + 1);
     else setIncorrect(Incorrect + 1);
-
   }
 
   function getCharClass(wordIdx, charIdx, char)          //assign color classess on the basis of wrong or right pressed word
   {
     if (wordIdx === currWordIndex && charIdx === currCharIndex && currChar && status != "finish") {
+      
       if (char === currChar) {
-        return "char_color_green";              //class for right char
+        return "char_color_green ";              //class for right char
       }
-      else return "char_color_red";             //class for wrong char
+      else return "char_color_red ";             //class for wrong char
     }
     else if (wordIdx === currCharIndex && currCharIndex >= words[currWordIndex].length) {
       return "char_color_red";
@@ -115,25 +126,38 @@ const Type_logic = () => {
     else return '';
 
   }
+ 
+  function getwordClass(wordIdx){
+    if(wordIdx===currWordIndex) return "worddd";
+  }
 
 
+  
   return (
     <>
-     
-      <div className="flex justify-center ... mt-9 ">                                 {/*timer component*/}
-        <h2 className="box-content  p-4 border-4 ... px-2 py-2">{countDown}</h2>
+
+
+
+          {/*timer component*/}
+          
+      <div className="flex justify-center ... mt-9 ">                               
+        <h2 className="box-content  p-4 border-4 ... px-2 py-2 text-purple-600 text-xl">
+  {countDown}</h2>
       </div>
 
 
      
+
+
+
          <>
          {/* paragraph  */}
          <div className="flex justify-center ... mt-12">
          <div className=" box-border h-auto w-10/12 p-4 border-4 ...  ">
             {words.map((word, i) => (                                  //assign i to every word of words array(random generated)
               <>
-                <span className="italic ... text-2xl font-medium tracking-normal" key={i}>
-                  <span>
+                <span className="italic ... text-2xl font-medium tracking-normal " key={i}>
+                  <span className={getwordClass(i)}>
                     {word.split("").map((char, idx) => (                //split each word of words array and assign idx
                       <span className={getCharClass(i, idx, char)} key={idx}>{char}</span>
                     ))}
@@ -149,6 +173,9 @@ const Type_logic = () => {
 
 
 
+
+
+         
       {/* input area componet */}
       <div className="flex justify-center ... mt-10">
 
@@ -157,28 +184,39 @@ const Type_logic = () => {
             <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20">
             ........</svg>
           </span>
-          <input ref={textInput} disabled={status != "started"} type="text" onKeyDown={handleKeyDown} value={currInput} onChange={(e) => setcurrInput(e.target.value)}  className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm italic ... text-2xl font-medium tracking-normal mt-7" placeholder="Write here" />
+          <input ref={textInput} disabled={status != "started"} type="text" onKeyDown={handleKeyDown} value={currInput} onChange={(e) => {
+                 setcurrInput(e.target.value);
+                 setValue(e.target.value);
+                 } 
+          }
+
+           className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm italic ... text-2xl font-medium tracking-normal mt-7 mr-5" placeholder="Write here" />
         </label>
 
 
 
 
 
+
+
         {/* contest start button */}
-        <div className="w-49 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 mt-6">
+        <div className="ml-5 w-49 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 mt-6">
           <button onClick={start}>start</button>
         </div>
       </div>
  
       
 
+
+
+
       {/* wpm and accuracy will be shown if contest is ended status ...finish.. */}
       {status === "finish" && (
-        <div className="grid gap-4 grid-cols-2">
+        <div className=" flex justify-center ...">
        
-            <div >WPM:{Correct}</div>
+            <div className=" text-purple-600 mr-20 mt-10  text-2xl font-medium tracking-normal">WPM : {Correct}</div>
 
-            <div >Accuracy:{Math.round((Correct / (Correct + Incorrect)) * 100)}</div>
+            <div className="text-purple-600 ml-20 mt-10  text-2xl font-medium tracking-normal" >Accuracy : {Math.round((Correct / (Correct + Incorrect)) * 100)}%</div>
       
         </div>
       )}
